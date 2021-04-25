1a7e433c6a48b612a732ff3842bd4619c7ef8ec4
M <stick100@hotmail.com>
2021-04-25 03:22:12 -0400

    Modify Axis Speed, and new Scene

---
+++ VRTKLD4848/Assets/Modify_Speed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zinnia.Data.Collection.List;
+
+public class Modify_Speed : MonoBehaviour
+{
+
+    public Vector3ObservableList mult;
+    public float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mult.enabled = true;
+        mult.RemoveAt(1);
+        mult.Add(new Vector3(speed, 0, speed));
+    }
+}
